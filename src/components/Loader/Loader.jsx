9e045ab009a5a2407f90b0ebdf840fef589df577
PopovaAnna1209 <popovaanna120987@gmail.com>
2023-06-01 18:36:36 +0300

    commit rhwif03_4

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,13 @@
-import { Vortex } from 'react-loader-spinner';
+import { InfinitySpin } from 'react-loader-spinner';
 import './Loader.css';
 
 export const Loader = () => {
   return (
     <div className="Loader">
-      <Vortex
-      visible={true}
-      height="80"
-      width="80"
-      ariaLabel="vortex-loading"
-      wrapperStyle={{}}
-      wrapperClass="vortex-wrapper"
-      colors={['red', 'green', 'blue', 'yellow', 'orange', 'purple']}
-      />
+      <InfinitySpin 
+  width='200'
+  color="#1294a5"
+/>
     </div>
   );
 };
